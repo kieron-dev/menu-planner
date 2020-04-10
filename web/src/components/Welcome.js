eedb07aabad5d0fd1c84e646421c5ce1025e2712
Kieron Browne <kbrowne@pivotal.io>
2020-04-10 22:03:33 +0100

    Welcome page with a google login button

---
+++ web/src/components/Welcome.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {
+  Segment,
+  Container,
+  Header
+} from 'semantic-ui-react';
+import GoogleLogin from 'react-google-login';
+
+const responseGoogle = (resp) => {
+  console.log(resp);
+};
+
+const Welcome = () => {
+  return (
+    <>
+      <Segment
+        inverted
+        textAlign='center'
+        vertical
+        style={{minHeight: 700}}
+      >
+        <Container text>
+          <Header
+            inverted
+            as="h1"
+            style={{fontSize: "4em", fontWeight: "normal", marginTop: "3em", marginBottom: "0"}}
+            >Menu-Planner</Header>
+          <Header
+            inverted
+            as="h2"
+            style={{fontSize: "1.7em", fontWeight: "normal", marginTop: "1.5em"}}
+      >Sign in to experience the delights!</Header>
+      <GoogleLogin
+        clientId="176462381984-bfq3v9mc00v0ipvpebiaiide4l22dmoh.apps.googleusercontent.com"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+      />
+
+        </Container>
+      </Segment>
+    </>
+  );
+};
+
+export default Welcome;
