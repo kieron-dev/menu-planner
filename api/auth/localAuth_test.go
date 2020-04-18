dac8e4e585ef029341448ca1cef45e316d2fbd3a
Kieron Browne <kbrowne@pivotal.io>
2020-04-18 21:55:38 +0100

    drive out the local auth / token generation behaviour
    Doesn't compile due to lack of a real UserStore

--- api/auth/localAuth_test.go
+++ api/auth/localAuth_test.go
@@ -1,23 +1,38 @@
 package auth_test
 
 import (
+	"errors"
+
 	"github.com/kieron-pivotal/menu-planner-app/auth"
-	"github.com/kieron-pivotal/menu-planner-app/jwt"
+	"github.com/kieron-pivotal/menu-planner-app/auth/authfakes"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("LocalAuth", func() {
 	var (
-		localAuth *auth.LocalAuth
-		email     string
-		name      string
-		token     string
-		err       error
+		localAuth        *auth.LocalAuth
+		fakeUserStore    *authfakes.FakeUserStore
+		fakeJWTGenerator *authfakes.FakeJWTGenerator
+		fakeUser         *authfakes.FakeUser
+		email            string
+		name             string
+		token            string
+		err              error
 	)
 
 	BeforeEach(func() {
-		localAuth = auth.NewLocalAuth()
+		fakeUserStore = new(authfakes.FakeUserStore)
+		fakeJWTGenerator = new(authfakes.FakeJWTGenerator)
+		fakeJWTGenerator.GenerateTokenReturns("abc.123.456", nil)
+		localAuth = auth.NewLocalAuth(fakeUserStore, fakeJWTGenerator)
+		fakeUser = new(authfakes.FakeUser)
+		fakeUser.IDReturns("abc123")
+		fakeUser.NameReturns(name)
+		fakeUserStore.FindByEmailReturns(fakeUser, nil)
+
+		email = "bob@example.com"
+		name = "Robert Ample"
 	})
 
 	JustBeforeEach(func() {
@@ -29,21 +44,86 @@ var _ = Describe("LocalAuth", func() {
 			Expect(err).NotTo(HaveOccurred())
 		})
 
-		It("returns a token", func() {
-			Expect(token).NotTo(BeEmpty())
+		It("checks the db for a row with the email", func() {
+			Expect(fakeUserStore.FindByEmailCallCount()).To(Equal(1))
+			actualEmail := fakeUserStore.FindByEmailArgsForCall(0)
+			Expect(actualEmail).To(Equal(email))
 		})
 
-		It("contains email and name in the claim set", func() {
-			claimSet, err := jwt.NewJWT().ClaimSet(token)
-			Expect(err).NotTo(HaveOccurred())
+		When("the user with that email exists already", func() {
+			It("doesn't create a new user", func() {
+				Expect(fakeUserStore.CreateCallCount()).To(Equal(0))
+			})
 
-			actualEmail, ok := claimSet["email"]
-			Expect(ok).To(BeTrue(), "email not present")
-			Expect(actualEmail).To(Equal(email))
+			It("uses the existing ID in the token generation", func() {
+				Expect(fakeJWTGenerator.GenerateTokenCallCount()).To(Equal(1))
+				actualID, _ := fakeJWTGenerator.GenerateTokenArgsForCall(0)
+				Expect(actualID).To(Equal("abc123"))
+			})
+		})
 
-			actualName, ok := claimSet["name"]
-			Expect(ok).To(BeTrue(), "name not present")
+		When("there is no existing user with that email", func() {
+			BeforeEach(func() {
+				fakeUserStore.FindByEmailReturns(nil, nil)
+				fakeUser.IDReturns("new234")
+				fakeUserStore.CreateReturns(fakeUser, nil)
+			})
+
+			It("creates a new user", func() {
+				Expect(fakeUserStore.CreateCallCount()).To(Equal(1))
+				actualEmail, actualName := fakeUserStore.CreateArgsForCall(0)
+				Expect(actualEmail).To(Equal(email))
+				Expect(actualName).To(Equal(name))
+			})
+
+			It("uses the new ID in the token generation", func() {
+				Expect(fakeJWTGenerator.GenerateTokenCallCount()).To(Equal(1))
+				actualID, _ := fakeJWTGenerator.GenerateTokenArgsForCall(0)
+				Expect(actualID).To(Equal("new234"))
+			})
+		})
+
+		It("uses the correct name in the JWT generation", func() {
+			Expect(fakeJWTGenerator.GenerateTokenCallCount()).To(Equal(1))
+			_, actualName := fakeJWTGenerator.GenerateTokenArgsForCall(0)
 			Expect(actualName).To(Equal(name))
+		})
+
+		It("returns the token", func() {
+			Expect(token).To(Equal("abc.123.456"))
+		})
+	})
+
+	Context("sad paths", func() {
+		When("find by email fails", func() {
+			BeforeEach(func() {
+				fakeUserStore.FindByEmailReturns(nil, errors.New("eek"))
+			})
+
+			It("returns the error", func() {
+				Expect(err).To(MatchError(ContainSubstring("eek")))
+			})
+		})
+
+		When("create user fails", func() {
+			BeforeEach(func() {
+				fakeUserStore.FindByEmailReturns(nil, nil)
+				fakeUserStore.CreateReturns(nil, errors.New("mmm"))
+			})
+
+			It("returns the error", func() {
+				Expect(err).To(MatchError(ContainSubstring("mmm")))
+			})
+		})
+
+		When("the token generation fails", func() {
+			BeforeEach(func() {
+				fakeJWTGenerator.GenerateTokenReturns("", errors.New("sorry"))
+			})
+
+			It("returns the error", func() {
+				Expect(err).To(MatchError(ContainSubstring("sorry")))
+			})
 		})
 	})
 })
--- api/auth/localauth.go
+++ api/auth/localauth.go
@@ -1,11 +1,55 @@
 package auth
 
-type LocalAuth struct{}
+import "fmt"
 
-func NewLocalAuth() *LocalAuth {
-	return &LocalAuth{}
+//counterfeiter:generate . User
+
+type User interface {
+	Email() string
+	Name() string
+	ID() string
 }
 
+//counterfeiter:generate . UserStore
+
+type UserStore interface {
+	FindByEmail(email string) (User, error)
+	Create(email, name string) (User, error)
+}
+
+//counterfeiter:generate . JWTGenerator
+
+type JWTGenerator interface {
+	GenerateToken(id, name string) (string, error)
+}
+
+type LocalAuth struct {
+	userStore    UserStore
+	jwtGenerator JWTGenerator
+}
+
+func NewLocalAuth(userStore UserStore, jwtGen JWTGenerator) *LocalAuth {
+	return &LocalAuth{
+		userStore:    userStore,
+		jwtGenerator: jwtGen,
+	}
+}
+
+// LocalAuth takes email and name from a trusted 3rd-party authenticator, e.g.
+// Google Sign-in, and produces a JWT for this system.
+//
+// It will return an ID from the database for the email, if a matching entry
+// exists, or will create a new entry in the database for the user.
 func (a *LocalAuth) LocalAuth(email, name string) (string, error) {
-	return "", nil
+	user, err := a.userStore.FindByEmail(email)
+	if err != nil {
+		return "", fmt.Errorf("local-auth failed looking up user %w", err)
+	}
+	if user == nil {
+		user, err = a.userStore.Create(email, name)
+		if err != nil {
+			return "", fmt.Errorf("local-auth failed creating new user %w", err)
+		}
+	}
+	return a.jwtGenerator.GenerateToken(user.ID(), name)
 }
