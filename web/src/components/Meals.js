ec0dd04ddc1cc5377a4eeec4848ea0ddb7552640
Kieron Browne <kbrowne@vmware.com>
2021-05-04 21:28:20 +0100

    Add multiple recipes to a meal
    Multi-select dropdown with additions.
    
    This breaks the Meal component. Might need to wrap the recipes in a
    provider making them accesible to Meals and Meal.

--- web/src/components/Meals.js
+++ web/src/components/Meals.js
@@ -1,28 +1,43 @@
 import React, { useState } from "react";
-import { Form, Card, Button } from "semantic-ui-react";
+import { Form, Card, Button, Dropdown } from "semantic-ui-react";
 import Meal from "./Meal";
-import v4 from "uuid";
+import { v4 } from "uuid";
 
 export default function Meals() {
-    const [name, setName] = useState("");
-
-    const [meals, setMeals] = useState(
-        ["spag bol", "lasagne"].map((n) => ({
-            id: v4(),
-            name: n,
-            description: "blah",
-        }))
+    const [mealRecipes, setMealRecipes] = useState([]);
+    const [recipes, setRecipes] = useState(
+        ["Spaghetti Bolognese", "Toad in the Hole", "Spicy Chicken Tagine"].map(
+            (r) => ({
+                key: r,
+                text: r,
+                value: v4(),
+            })
+        )
     );
 
-    const changeName = (e) => {
-        setName(e.target.value);
+    const [meals, setMeals] = useState([]);
+
+    const changeName = (_, { value }) => {
+        console.log("value", value);
+        setMealRecipes(value);
+    };
+
+    const handleAddition = (_, { value }) => {
+        const newId = v4();
+        const newRecipes = recipes.concat({
+            key: value,
+            text: value,
+            value: newId,
+        });
+        setRecipes(newRecipes);
+        setMealRecipes(mealRecipes.concat(newId));
     };
 
     const addMeal = (e) => {
         e.preventDefault();
-        const newMeals = meals.concat({ id: v4(), name: name });
+        const newMeals = meals.concat({ id: v4(), name: mealRecipes });
         setMeals(newMeals);
-        setName("");
+        setMealRecipes([]);
     };
 
     const removeMeal = (id) => {
@@ -33,12 +48,21 @@ export default function Meals() {
     return (
         <>
             <Form onSubmit={addMeal} style={{ marginBottom: "5ex" }}>
-                <Form.Input
-                    label="Recipe"
-                    placeholder="Recipe"
-                    value={name}
-                    onChange={changeName}
-                />
+                <Form.Field>
+                    <Dropdown
+                        placeholder="Add recipes"
+                        value={mealRecipes}
+                        options={recipes}
+                        onChange={changeName}
+                        onAddItem={handleAddition}
+                        fluid
+                        search
+                        selection
+                        multiple
+                        allowAdditions
+                        additionPosition="bottom"
+                    />
+                </Form.Field>
                 <Button type="submit">Add Meal</Button>
             </Form>
 
